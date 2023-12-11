8badb337a4d6b720eabda5df846182aafdcd4f7a
Gilt457 <lepatioborel@gmail.com>
2023-12-11 10:23:27 -0800

    hacking injection

---
+++ 0x18-dynamic_libraries/hack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * printf - prints formatted output to stdout
+ * @format: A pointer to the format string containing the placeholders
+ * Return: The number of characters printed (excluding the null byte)
+ */
+int printf(const char *format, ...)
+{
+write(1, "9 8 10 24 75 +9\n", 17);
+write(1, "Congratulations, you win the Jackpot!\n", 38);
+
+exit(EXIT_SUCCESS);
+}
