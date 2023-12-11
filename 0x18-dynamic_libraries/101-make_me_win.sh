0ec0c40b7dca7198e7ee515290e4291ada2d7333
Gilt457 <lepatioborel@gmail.com>
2023-12-11 10:48:49 -0800

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wget -p
+wget -P .. 
+https://raw.githubusercontent.com/Gilt457/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
